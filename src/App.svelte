4d1c436f1706b71d2db5400cfaa41eebda57329b
Supergamer1337 <felix.b.aronsson@gmail.com>
2021-05-24 13:00:07 +0200

    Added SVG and created Card

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,9 @@
 	import RoundCard from './cards/RoundCard.svelte';
 	import BoostsCard from './cards/BoostsCard.svelte';
 	import AddBoostsCard from './cards/AddBoostsCard.svelte';
+	import CombatantsCard from './cards/CombatantsCard.svelte';
 </script>
 
-<AddBoostsCard />
-<BoostsCard />
+<CombatantsCard />
+
+<RoundCard />
---
+++ src/cards/CombatantsCard.svelte
@@ -0,0 +1,43 @@
+<script>
+	import Fa from 'svelte-fa';
+	import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus';
+</script>
+
+<div class="card">
+	<div class="header-container">
+		<p class="section-header">Players</p>
+		<Fa icon={faPlus} />
+	</div>
+
+	<div class="header-container">
+		<p class="section-header">Enemies</p>
+		<Fa icon={faPlus} />
+	</div>
+</div>
+
+<style>
+	.card {
+		background-color: var(--secondary-background);
+		color: var(--primary-text);
+		width: 50%;
+		margin: 1em auto;
+		padding: 2em;
+		border-radius: var(--primary-rounding);
+	}
+
+	.section-header {
+		font-size: 1.4rem;
+		font-weight: 600;
+	}
+
+	.header-container {
+		display: flex;
+		align-items: center;
+	}
+
+	.header-container :global(svg) {
+		margin-left: 1em;
+		cursor: pointer;
+		font-size: 1.2rem;
+	}
+</style>
